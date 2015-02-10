57df23eb97470ef177aad19a9ea7f57e76a2b86e
Chloe <Chloe@Chloes-MBP.phub.net.cable.rogers.com>
2015-02-09 19:26:57 -0500

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Chloe on 2015-02-09.
+//  Copyright (c) 2015 BNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Chloe on 2015-02-09.
+//  Copyright (c) 2015 BNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
